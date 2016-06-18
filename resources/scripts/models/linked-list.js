fce0340da394a3e60a36aeb03765417a6bda0781
Kyle Gullicksen <noobtastic101@gmail.com>
2016-06-17 17:49:50 -0700

    setPointer skelton in place (sans cycle handling)
    Cycle handling will comense presently (consider this a save point before that occurs on a simplistic build of setPointer)

--- resources/scripts/models/linked-list.js
+++ resources/scripts/models/linked-list.js
@@ -1,10 +1,21 @@
 (function ($, _, Backbone, app) {
 
+function NodeNotFoundException(id, message) {
+	this.id = id;
+	this.message = "";
+
+	if(message !== null) {
+		this.message = message;
+	}
+}
+
 app.models.LinkedListNode = Backbone.Model.extend({
 	defaults: {
 		elem: null,
-		next: null
+		next: null,
+		id : null
 	},
+
 	initiailize: function (elem) {
 		this.set('elem', elem);
 	}
@@ -16,44 +27,82 @@ app.models.LinkedList = Backbone.Model.extend({
 		rear: null,
 	},
 	initialize: function () {
-		this.set('nodes', []);
+		this.set('nodes', {}); //node id -> node object
+		this.set('ordered_unreachable_nodes' []);  //array of node objects
+		this.set('pointer_counter' {}); //node id -> int (count)
 	},
-	setPointer: function (srcPointerName, dstPointerName) {
-		var dstPointer;
-		if (dstPointerName === 'front') {
-			dstPointer = this.get('front');
-		} else if (dstPointerName === 'rear') {
-			dstPointer = this.get('rear');
-		} else if (dstPointerName === 'null') {
-			dstPointer = null;
-		} else if (dstPointerName === 'new Node') {
-			dstPointer = new app.models.LinkedListNode({
-				elem: 42
-			});
-			this.get('nodes').push(dstPointer);
+
+
+	setPointer: function (srcNodeId, dstNodeId) {
+		srcNode = getNode(srcNodeId);
+		dstNode = getNode(dstNodeId);
+
+		//validate...
+		if(srcNode === undefined) {
+			throw new NodeNotFoundException(srcNodeId);
 		}
-		if (srcPointerName === 'front') {
-			this.set('front', dstPointer);
-		} else if (srcPointerName === 'rear') {
-			this.get('rear', dstPointer);
+		else if(dstNode === undefined) {
+			throw new NodeNotFoundException(dstNodeId);
+		}
+
+		//is there anything for us to do?
+		if(srcNode.next === dstNode)
+			return; //nope
+
+		//will changing pointers result in nodes becoming unreachable?
+		//[hopefully this is quick enough procedure]
+		if(srcNode.next !== null) {
+			newlyUnreachableNodes = getNewlyUnreachableNodes(srcNode, dstNode);
+			this.ordered_unreachable_nodes.push.apply(newlyUnreachableNodes);
+		}
+
+		//apply the requested change of pointers
+		srcNode.next = dstNode;
+	},
+
+	getNode : function (id) {
+		//NULL is indeed a valid node
+		if(id === "null") {
+			return null;
+		}
+		else if(this.nodes.hasOwnProperty(id)) {
+			return this.nodes.id;
+		}
+		else {
+			return undefined;
 		}
 	},
+
+	getNewlyUnreachableNodes : function (srcNode, dstNode) {
+
+	}
+
+
+
 	initializeExample: function () {
 		var node3 = new app.models.LinkedListNode({
-			elem: 99
+			elem: 99,
+			id: '2'
 		});
 		var node2 = new app.models.LinkedListNode({
 			elem: 42,
-			next: node3
+			next: node3,
+			id: '1'
 		});
 		var node1 = new app.models.LinkedListNode({
 			elem: 24,
-			next: node2
+			next: node2,
+			id: '0'
 		});
-		var nodes = [node1, node2, node3];
+		//var nodes = [node1, node2, node3];
+		var nodes = {
+			'0' : node1,
+			'1' : node2,
+			'2' : node3
+		}
 		this.set('nodes', nodes);
-		this.set('front', nodes[0]);
-		this.set('', nodes[nodes.length - 1]);
+		this.set('front', nodes['0']);
+		this.set('', nodes[2]);
 	}
 });
 
