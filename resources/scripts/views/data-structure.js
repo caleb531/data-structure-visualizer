616341b3b23061c51bef44ea11eb73767f28dc48
Caleb Evans <caleb@calebevans.me>
2016-06-27 17:16:39 -0700

    Make canvas pannable

--- resources/scripts/views/data-structure.js
+++ resources/scripts/views/data-structure.js
@@ -3,8 +3,15 @@
 app.views.DataStructure = Backbone.View.extend({
 	initialize: function () {
 		this.$el.empty();
-		this.paper = Raphael(this.el, 800, 300);
+		this.paper = Raphael(this.el, '100%', 300);
 		this.render();
+		// Allow user to pan canvas
+		this.$el.panzoom({
+			disableZoom: false
+		});
+		var panzoom = this.$el.panzoom('instance');
+		panzoom.$elem = this.$el.children('svg');
+		panzoom.elem = panzoom.$elem[0];
 	},
 });
 
