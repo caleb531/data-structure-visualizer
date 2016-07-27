0c4bcb1f5e8a47cec4a44c4e8aa24ba5312f9bb3
Caleb Evans <caleb@calebevans.me>
2016-07-27 10:31:37 -0700

    Add documentation comments to DataStructure view

--- resources/scripts/views/data-structure.js
+++ resources/scripts/views/data-structure.js
@@ -1,10 +1,15 @@
 (function () {
 
+// The parent view from which all data structure views MUST inherit; this view
+// must remain accessible under the global app.views object so that child views
+// can access it
 app.views.DataStructure = Backbone.View.extend({
+	// Initialize the view (this function is automatically called by Backbone
+	// when a new child view is instantiated)
 	initialize: function () {
 		this.$el.empty();
 		this.canvas = Raphael(this.el, '100%', 340);
-		// Allow user to pan canvas
+		// Allow user to pan canvas via user interaction on the container
 		this.$el.panzoom({
 			disableZoom: true
 		});
@@ -12,7 +17,8 @@ app.views.DataStructure = Backbone.View.extend({
 		this.panzoom.$elem = this.$el.children('svg');
 		this.panzoom.elem = this.panzoom.$elem[0];
 	},
-	// Recenter canvas translation to 0,0
+	// Center the inner canvas within the outer container (where the data
+	// structure will always be visible)
 	recenterCanvas: function () {
 		this.panzoom.reset({
 			animate: false
